39ad8dd25dcf4970a8fcc2978ca6c1812aaeadbd
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-03 23:41:51 +0100

    fix: Add way to change calculator display

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,27 @@
 "use strict";
 
+const displayElement = document.getElementById("display");
+const displayValues = {
+  firstNumber: null,
+  operator: null,
+  secondNumber: null,
+};
+
+function updateDisplay() {
+  if (displayValues.firstNumber === null) {
+    displayElement.classList.add("empty");
+    displayElement.textContent = "0";
+    return;
+  }
+
+  displayElement.classList.remove("empty");
+  displayElement.textContent = displayValues.firstNumber;
+  if (displayValues.operator === null) return;
+  displayElement.textContent += " " + displayValues.operator;
+  if (displayValues.secondNumber === null) return;
+  displayElement.textContent += " " + displayValues.secondNumber;
+}
+
 function addKeyBindings() {
   window.addEventListener("keyup", (event) => {
     let button = document.querySelector(
@@ -21,6 +43,7 @@ function simulateClick(button) {
 
 function main() {
   addKeyBindings();
+  updateDisplay();
 }
 
 main();
