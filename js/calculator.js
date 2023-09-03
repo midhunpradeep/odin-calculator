f9f4efd381bfded68ad774d77914c398ae8b312d
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-03 23:33:52 +0100

    feat: Add keybindings

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,26 @@
 "use strict";
 
-function main() {}
+function addKeyBindings() {
+  window.addEventListener("keyup", (event) => {
+    let button = document.querySelector(
+      `#btn-container button[data-key="${event.key.toLowerCase()}"]`,
+    );
+    if (button) {
+      simulateClick(button);
+    }
+  });
+}
+
+function simulateClick(button) {
+  button.classList.add("active");
+  button.click();
+  setTimeout(() => {
+    button.classList.remove("active");
+  }, 100);
+}
+
+function main() {
+  addKeyBindings();
+}
 
 main();
